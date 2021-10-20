aa4bd2086b815852ccdde01ecced24d22c6df0fd
crisantudor79@yahoo.com <crisantudor79@yahoo.com>
2021-10-20 23:35:14 +0300

    data for: free to play champs and news (no design))

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,50 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/"></router-link> |
+    <!--<router-link to="/about"></router-link>-->
 
     <router-view />
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import url("https://fonts.googleapis.com/css2?family=Poppins&display=swap");
+html {
+  font-size: 62.5%;
+  scroll-behavior: smooth;
+  overflow-x: hidden;
+
+  @media screen and (max-width: 75em) {
+    font-size: 56.25%;
+  }
+
+  @media screen and (max-width: 56.25em) {
+    font-size: 50%;
+  }
+
+  @media screen and (max-width: 37.5em) {
+    font-size: 50%;
+  }
+
+  @media screen and (min-width: 121.875em) {
+    font-size: 65%;
+  }
+}
+body {
+  font-family: "Poppins", sans-serif;
+  font-weight: 400;
+  line-height: 1.7;
+  overflow-x: hidden;
+  box-sizing: border-box;
+}
+* {
+  margin: 0;
+  padding: 0;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+*,
+*::after,
+*::before {
+  box-sizing: inherit;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,7 +14,41 @@
       v-model="name"
       @keyup.enter="getData()"
     />
-    {{ int.dataAccount }}
+
+    <button @click="getData()">Search</button>
+
+    <div class="free_to_play">
+      <div v-for="i in int.dataFreeChamps.freeChampionIds" :key="i">
+        <img
+          :src="
+            'https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-icons/' +
+            i +
+            '.png'
+          "
+          alt=""
+          width="100"
+          height="100"
+        />
+      </div>
+    </div>
+
+    <div class="news">
+      <div v-for="i in news" :key="i">
+        <a class="news_article news_1" :href="i.link" target="_blank">
+          <img
+            :src="i.img"
+            alt="patch_notes"
+            class="news_img"
+            width="200"
+            height="200"
+          />
+          <div class="news_article_details">
+            <h3 class="news_article_game asd1">{{ i.name }}</h3>
+            <p class="news_article_description">{{ i.description }}</p>
+          </div>
+        </a>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -35,7 +69,7 @@ export default {
     const region = ref(""); // selected region
     const int = reactive({
       //data
-      dataFreeChamps: null,
+      dataFreeChamps: "",
       dataAccount: null,
     });
 
@@ -53,17 +87,53 @@ export default {
       { name: "JP", value: "jp1" },
     ]);
 
+    const news = ref([
+      {
+        link: "https://www.leagueoflegends.com/en-us/news/game-updates/patch-11-21-notes/",
+        name: "LEAGUE OF LEGENDS",
+        description: "Patch 10.21 notes",
+        img: "https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/bltb16f7bf3691493f5/5e32376ae147ae4537d93a32/LOL_PROMOART_5.jpg",
+      },
+      {
+        link: "https://lolesports.com/",
+        name: "LEAGUE OF LEGENDS",
+        description: "Watch WORLDS 2021 for drops",
+        img: "https://images.contentstack.io/v3/assets/bltad9188aa9a70543a/blt69c9ddbcdc18229b/616fe86bb05858694858be72/ArticleHeader_Lolincinemas.png?width=1600&height=900",
+      },
+      {
+        link: "https://www.leagueoflegends.com/en-us/news/game-updates/teamfight-tactics-patch-11-21-notes/",
+        name: "TEAMFIGHT TACTICS",
+        description: "TFT patch 10.21 notes",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhOX7B0dohDs9FpWGcJaI6qlR9lH40ydP7-A&usqp=CAU",
+      },
+      {
+        link: "https://www.leagueoflegends.com/en-us/news/game-updates/preseason-2022-pbe-preview/",
+        name: "LEAGUE OF LEGENDS",
+        description: "PRESEASON 2022: PBE PREVIEW",
+        img: "https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/blt2a24c87c7ab9e0bf/616a827d35be917f1d1fc0a7/101921_Preseason2022_Banner.jpg",
+      },
+      {
+        link: "https://wol.gg/",
+        name: "LEAGUE OF LEGENDS",
+        description: "See how much time you wasted in lol",
+        img: "https://www.esports.net/wp-content/uploads/2020/02/wol-gg-wasted-on-lol-homepage.jpg",
+      },
+      {
+        link: "https://www.leagueoflegends.com/en-us/news/game-updates/disabling-all-chat/",
+        name: "LEAGUE OF LEGENDS",
+        description: "DISABLING /ALL CHAT",
+        img: "https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/blt0a67287cb5aba60b/6160b7a7aeba391136e44814/Behavioral_Systems_September_Banner_(1).jpg",
+      },
+      {
+        link: "https://www.leagueoflegends.com/en-us/news/game-updates/ranked-rewards-2021/",
+        name: "LEAGUE OF LEGENDS",
+        description: "RANKED REWARDS 2021",
+        img: "https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/bltae3bfdef2414090b/615b3aa5e289fa55888ed8f3/Victorious_Blitzcrank_1920.jpg",
+      },
+    ]);
+
     function getData() {
       //get data from backend
-      console.log("salut");
-
-      axios(`http://localhost:3000/champion-v3`) // free champs rotation
-        .then((res) => {
-          int.dataFreeChamps = res.data;
-          console.log(res.data);
-        });
-      //console.log(region.value)
-
       axios(`http://localhost:3000/summoner-v4/${region.value}/${name.value}`) // account details
         .then((res) => {
           int.dataAccount = res.data;
@@ -71,13 +141,30 @@ export default {
         });
     }
 
+    function getFreeToPlay() {
+      axios(`http://localhost:3000/champion-v3`) // free champs rotation
+        .then((res) => {
+          int.dataFreeChamps = res.data;
+          console.log(res.data);
+        });
+    }
+
     return {
       int,
       getData,
+      getFreeToPlay,
       name,
       region,
       regions,
+      news,
     };
+  },
+  mounted() {
+    this.getFreeToPlay();
   },
 };
 </script>
+
+<style lang="scss">
+@import "../styles/search_page.scss"; // search_page styles
+</style>
