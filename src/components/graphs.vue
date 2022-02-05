8b21a94dab9d1bad351ae2aeede781f9c46fb46f
crisantudor79@yahoo.com <crisantudor79@yahoo.com>
2022-02-06 00:50:03 +0200

    graphs component

---
+++ src/components/graphs.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+      <p>grafice</p>
+  </div>
+</template>
+
+<script>
+import { reactive } from "vue";
+export default {
+    name: "graphs",
+    props: {
+        timeline: Object,
+    },
+    setup(){
+        const data = reactive({
+            test:null,
+        });
+
+        function getData(){
+            console.log(this.timeline);
+        }
+
+        return{
+            data,
+            getData,
+        }
+    },
+    mounted(){
+        this.getData();
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+
+</style>
